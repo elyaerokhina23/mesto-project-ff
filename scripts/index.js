43e13bb07579a2fc918b8f9b15ede0142f37d403
Eleonora Erokhina <elya.erokhina23@mail.ru>
2024-12-04 19:45:12 +0300

    create: реализация добавления и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".places__item");
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCallback) {
+  const card = cardTemplate.cloneNode(true);
+  const cardTitle = card.querySelector(".card__title");
+  const cardImage = card.querySelector(".card__image");
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+  const deleteButton = card.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteCallback(card);
+  });
+  return card;
+}
 
 // @todo: Функция удаления карточки
+function cardDelete(card) {
+  card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, cardDelete);
+  placesList.append(cardElement);
+});
